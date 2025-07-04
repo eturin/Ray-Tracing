26aceb241f6c189858d832ac1000c734dabcb1ac
ture <eturin@gmail.com>
2025-07-04 20:43:16 +0300

    Init

--- chap-1/target/debug/chap-1.d
+++
@@ -1,1 +0,0 @@
-/home/ture/work/rust/Ray-Tracing/chap-1/target/debug/chap-1: /home/ture/work/rust/Ray-Tracing/chap-1/src/main.rs
--- chap-1/target/debug/deps/chap_1-5fcf81ad9276030e.d
+++
@@ -1,5 +0,0 @@
-/home/ture/work/rust/Ray-Tracing/chap-1/target/debug/deps/chap_1-5fcf81ad9276030e: src/main.rs
-
-/home/ture/work/rust/Ray-Tracing/chap-1/target/debug/deps/chap_1-5fcf81ad9276030e.d: src/main.rs
-
-src/main.rs:
