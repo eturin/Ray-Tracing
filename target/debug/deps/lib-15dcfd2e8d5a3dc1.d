222411da31c769c4c540d101807b8367bff81388
ture <eturin@gmail.com>
2025-07-04 21:24:42 +0300

    fix

--- target/debug/deps/lib-15dcfd2e8d5a3dc1.d
+++
@@ -1,8 +0,0 @@
-/home/ture/work/rust/Ray-Tracing/target/debug/deps/liblib-15dcfd2e8d5a3dc1.rmeta: chap-2/src/lib/lib.rs chap-2/src/lib/vec3.rs
-
-/home/ture/work/rust/Ray-Tracing/target/debug/deps/liblib-15dcfd2e8d5a3dc1.rlib: chap-2/src/lib/lib.rs chap-2/src/lib/vec3.rs
-
-/home/ture/work/rust/Ray-Tracing/target/debug/deps/lib-15dcfd2e8d5a3dc1.d: chap-2/src/lib/lib.rs chap-2/src/lib/vec3.rs
-
-chap-2/src/lib/lib.rs:
-chap-2/src/lib/vec3.rs:
--- target/debug/liblib.d
+++
@@ -1,1 +0,0 @@
-/home/ture/work/rust/Ray-Tracing/target/debug/liblib.rlib: /home/ture/work/rust/Ray-Tracing/chap-2/src/lib/lib.rs /home/ture/work/rust/Ray-Tracing/chap-2/src/lib/vec3.rs
