9584921a642c5f0c2c284311731b0abaa43a21c2
ture <eturin@gmail.com>
2025-07-08 19:19:04 +0300

    fix

--- chap-8-2/src/lib/sphere/tests.rs
+++ chap-8-2/src/lib/sphere/tests.rs
@@ -1,9 +1,10 @@
+use crate::metal::Metal;
 use crate::sphere::Sphere;
 use crate::vec3::Vec3;
 
 #[test]
 fn test_base() {
-    let a = Sphere::new(Vec3::new(1., 2., 3.), 7.);
+    let a = Sphere::new(Vec3::new(1., 2., 3.), 7.,Some(Box::new(Metal::new(Vec3::new(0.4, 0.7, 0.3), 0.1))));
     assert_eq!(a.radius, 7.);
     assert_eq!(a.center[0], 1.);
 }
